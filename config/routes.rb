29925c92dfd641ca80936bf3cef55c827a27ec5f
Jair <j.irc.26@gmail.com>
2017-03-23 15:05:10 -0600

    Api functionality and versioning through headers

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,13 @@
+require 'api_constraints'
+
 Rails.application.routes.draw do
   #Api definition
 
   namespace :api, defaults: { format: :json }, constraints: { subdomain: 'api' }, path: '/' do
-    #Resourcers here
+
+    scope module: :v1, constraints: ApiConstraints.new(version: 1, default: true) do
+      #Resourcers here
+    end
   end
 
 end
